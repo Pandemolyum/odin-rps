ef917decdd99ffc60fdfdc19846a5eb528b69182
Pandemolyum <77362203+Pandemolyum@users.noreply.github.com>
2025-10-02 20:00:16 -0400

    Refactor code to display info in window
    Previously the game info was displayed in the console. It can now be
    viewed directly in the browser window and played by clicking buttons.

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // A game of rock paper scissors that is played in the console.
+const allButtons = document.querySelectorAll('button');
 
-playGame();
+for (button of allButtons) {
+    button.addEventListener('click', (e) => playGame(e.target.textContent));
+}
 
+const h1 = document.querySelector('h1');
+const h2 = document.querySelector('h2');
+const result = document.querySelector('.result');
+const score = document.querySelector('.score');
+
+let humanScore = 0;
+let compScore = 0;
+
+// ============ Functions ============
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3);
 
@@ -15,56 +27,51 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    return prompt("What do you pick? Rock, paper, or scissors?")
-}
-
 function playRound(humanChoice, compChoice) {
     humanChoice = humanChoice.toLowerCase();
 
     if (humanChoice === compChoice) {
-        console.log("It's a tie!")
+        result.textContent = "It's a tie!"
         return -1;
     } else if (humanChoice === "rock" && compChoice === "paper") {
-        console.log("You lose! Paper beats Rock.")
+        result.textContent = "You lose! Paper beats Rock."
         return 0;
     } else if (humanChoice === "rock" && compChoice === "scissors") {
-        console.log("You win! Rock beats Scissors.")
+        result.textContent = "You win! Rock beats Scissors."
         return 1;
     } else if (humanChoice === "paper" && compChoice === "rock") {
-        console.log("You win! Paper beats Rock.")
+        result.textContent = "You win! Paper beats Rock."
         return 1;
     } else if (humanChoice === "paper" && compChoice === "scissors") {
-        console.log("You lose! Scissors beat Paper.")
+        result.textContent = "You lose! Scissors beat Paper."
         return 0;
     } else if (humanChoice === "scissors" && compChoice === "rock") {
-        console.log("You lose! Rock beats Scissors.")
+        result.textContent = "You lose! Rock beats Scissors."
         return 0;
     } else if (humanChoice === "scissors" && compChoice === "paper") {
-        console.log("You win! Scissors beat Paper.")
+        result.textContent = "You win! Scissors beat Paper."
         return 1;
     } else {
-        console.log("You lose. Invalid choice.")
+        result.textContent = "You lose. Invalid choice."
         return 0;
     }
 }
 
-function playGame() {
-    let humanScore = 0;
-    let compScore = 0;
+function playGame(humanChoice) {
+    let compChoice = getComputerChoice();
+    let result = playRound(humanChoice, compChoice);
 
-    for (let i = 0; i < 5; i++) {
-        let compChoice = getComputerChoice();
-        let humanChoice = getHumanChoice();
+    if (result === 1) {
+        humanScore++;
+    } else if (result === 0) {
+        compScore++;
+    }
 
-        let result = playRound(humanChoice, compChoice);
-
-        if (result === 1) {
-            humanScore++;
-        } else if (result === 0) {
-            compScore++;
-        }
-
-        console.log("The score is " + humanScore + "-" + compScore);
+    if (humanScore >= 5) {
+        score.textContent = "The score is " + humanScore + "-" + compScore + ". You won!";
+    } else if (compScore >= 5) {
+        score.textContent = "The score is " + humanScore + "-" + compScore + ". You lost :(";
+    } else {
+        score.textContent = "The score is " + humanScore + "-" + compScore;
     }
 }
